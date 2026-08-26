9e4e41172d6bcbc424467231f91938a841dbc31d
Muhammad Taimoor <mtaimoor1999@gmail.com>
2026-08-27 00:52:31 +0500

    Restored previous version when the app was running fine

--- All App Info/fluent-dynamic-pages/test_fields.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('/Users/muhammadtaimoor/Desktop/Custom webpage/wp-load.php');
-global $wpdb;
-$form = $wpdb->get_row("SELECT form_fields FROM {$wpdb->prefix}fluentform_forms ORDER BY id DESC LIMIT 1");
-if ($form) {
-    print_r(json_decode($form->form_fields, true));
-}
--- temp_plugin_code/test_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('../../wp-load.php');
-global $wpdb;
-$row = $wpdb->get_row("SELECT response FROM {$wpdb->prefix}fluentform_submissions ORDER BY id DESC LIMIT 1");
-if ($row) {
-    print_r(array_keys(json_decode($row->response, true)));
-}
